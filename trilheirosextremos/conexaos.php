3ed480bd5a7242f56df01c5bfcb0c417a63652a6
marcio-jsb <107966905+marcio-jsb@users.noreply.github.com>
2025-05-28 02:09:57 -0300

    Add files via upload

---
+++ trilheirosextremos/conexaos.php
@@ -0,0 +1,10 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$senha ="";
+$dbname = "trilheiros";
+
+$conn = mysqli_connect($servidor,$usuario,$senha,$dbname);
+
+
+?>
---
+++ trilheirosextremos/conexaosl.php
@@ -0,0 +1,11 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "trilheiros";
+
+$mysqli = new mysqli($servidor,$usuario,$senha,$dbname);
+
+
+
+?>
